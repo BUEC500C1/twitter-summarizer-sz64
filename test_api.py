2e13be1e0fc167c8422883b19923614228cdbd02
Steven <sz64@bu.edu>
2020-02-03 16:53:51 -0500

    Added more tests

--- test_api.py
+++ test_api.py
@@ -1,4 +1,19 @@
 import weather_api as w
 
-def test_valid():
+def test_valid_getcoord():
 	assert ('140.386001587', '35.7647018433') == w.get_coord("Narita International AiRport");
+	assert ('140.386001587', '35.7647018433') == w.get_coord("NARITA INTERNATIONAL AIRPORT");
+	assert ('140.386001587', '35.7647018433') == w.get_coord("narita international airport");
+	
+def test_invalid_getcoord():
+	assert ('140.386001587', '35.7647018433') == w.get_coord("NARITA AIRPORT");
+	assert ('140.386001587', '35.7647018433') == w.get_coord("NARITA INTERNATIONAL AIRPORT ");
+	assert ('140.386001587', '35.7647018433') == w.get_coord("NARITA1 INTERNATIONAL AIRPORT");
+	
+def test_valid_temp():
+	assert isnumeric(w.get_temp(-95.622497558594,39.068698883057));
+	
+def test_invalid_temp():
+	assert "Error" == w.get_temp('a','b');
+	assert "Error" == w.get_temp('-100','-200');
+	assert "Error" == w.get_temp('///','12');
